422d243fabf04c7bb3da7dcf427d72a3d4fd8393
rafaballerini <rafaella_ballerini@hotmail.com>
2021-05-27 19:33:23 -0300

    localstorage parte index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
 const linguagem = document.querySelector('.js-linguagem')
 const areaDoCodigo = document.querySelector('.js-codigo-wrapper')
 const botaoPreview = document.querySelector('.js-botao-preview')
+const botaoSalvar = document.querySelector('.js-botao-salvar')
+const tituloProjeto = document.querySelector('.js-titulo-projeto')
+const descricaoProjeto = document.querySelector('.js-descricao-projeto')
 
 botaoPreview.addEventListener('click', () => {
     let codigo = areaDoCodigo.querySelector('code')
@@ -15,4 +18,50 @@ function mudaLinguagem() {
     let codigo = {'texto': areaDoCodigo.querySelector('code').innerText}
     areaDoCodigo.innerHTML = `<code class="preview hljs ${linguagem.value}" contenteditable="true" aria-label="editor"></code>`
     areaDoCodigo.firstChild.innerText = codigo.texto
+}
+
+botaoSalvar.addEventListener('click', () => {
+    if (typeof(Storage) !== "undefined"){
+        console.log("suporta o localstorage")
+        const projeto = montaProjeto()
+        salvaLocalStorage(projeto)
+        console.log(projeto)
+    }else{
+        console.log("não suporta o localstorage")
+    }
+})
+
+let numeroId = 1
+
+if (localStorage.length > 0) {
+    numeroId = localStorage.length
+}
+
+function montaProjeto(){
+    let projeto = {
+        'id': atribuiId(),
+        'detalhesDoProjeto':{
+            'nomeDoProjeto': tituloProjeto.value,
+            'descricaoDoProjeto': descricaoProjeto.value,
+            'linguagem': linguagem.value,
+            'codigo': areaDoCodigo.querySelector('code').innerText
+        }
+    }
+    return projeto
+}
+
+function atribuiId(){
+    if (localStorage.length == 0){
+        return 0
+    } else {
+        if (localStorage.length == numeroId){
+            let novoId = numeroId
+            numeroId++
+            return novoId        
+        }
+    }
+}
+
+function salvaLocalStorage(objetoJson){
+    localStorage.setItem(objetoJson.id, JSON.stringify(objetoJson))
 }
