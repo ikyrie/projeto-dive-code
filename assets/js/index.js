572f001a9f673077b81d24a69436dbe0ccf2d47f
ikyrie <mah.c5r@gmail.com>
2021-05-27 16:09:49 -0300

    Preparando o projeto para o divecode

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,6 @@
 const linguagem = document.querySelector('.js-linguagem')
 const areaDoCodigo = document.querySelector('.js-codigo-wrapper')
-const tituloProjeto = document.querySelector('.js-titulo-projeto')
-const descricaoProjeto = document.querySelector('.js-descricao-projeto')
 const botaoPreview = document.querySelector('.js-botao-preview')
-const botaoSalvar = document.querySelector('.js-botao-salvar')
 
 botaoPreview.addEventListener('click', () => {
     let codigo = areaDoCodigo.querySelector('code')
@@ -18,49 +15,4 @@ function mudaLinguagem() {
     let codigo = {'texto': areaDoCodigo.querySelector('code').innerText}
     areaDoCodigo.innerHTML = `<code class="preview hljs ${linguagem.value}" contenteditable="true" aria-label="editor"></code>`
     areaDoCodigo.firstChild.innerText = codigo.texto
-}
-
-botaoSalvar.addEventListener('click', () => {
-    if (typeof(Storage) !== "undefined") {
-        console.log('Yay, support!')
-        const projeto = montaProjeto()
-        salvaLocalStorage(projeto)
-    } else {
-        console.log('Nay, no support!')
-    }
-})
-
-function montaProjeto() {
-    let projeto = {
-        'id': atribuiId(),
-        'detalhesDoProjeto': {
-            'nomeDoProjeto': tituloProjeto.value,
-            'descricaoDoProjeto': descricaoProjeto.value,
-            'linguagem': linguagem.value,
-            'codigo': areaDoCodigo.querySelector('code').innerText
-        }
-    }
-    return projeto
-}
-
-let numeroId = 1
-
-if(localStorage.length > 0) {
-    numeroId = localStorage.length
-}
-
-function atribuiId() {
-    if(localStorage.length == 0) {
-        return 0
-    } else {
-        if(localStorage.length == numeroId) {
-            let novoId = numeroId
-            numeroId++
-            return novoId
-        }
-    }
-}
-
-function salvaLocalStorage(objetoJson) {
-    localStorage.setItem(objetoJson.id, JSON.stringify(objetoJson))
 }
